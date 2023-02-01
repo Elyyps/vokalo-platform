ba74af87746a14818611ef93c12fdb60e332b214
ahmed <ahmed@vokalo.io>
2023-02-01 11:38:44 +0100

    fix: fixed pitch view

--- src/components/modules/field-audio-overview/field-audio-overview.tsx
+++ src/components/modules/field-audio-overview/field-audio-overview.tsx
@@ -23,10 +23,7 @@ export const FieldAudioOverviewComponent = ({
 }: IFieldOverviewComponent) => {
   const sortPlayers = (list: any[]) => {
     if (list.length) {
-      const filtertedData = list.filter(
-        (player: any) => player.profile.gridX >= 0 && player.profile.gridY >= 0
-      );
-      return filtertedData.sort((a, b) => {
+      return list.sort((a, b) => {
         if (a.profile.gridX === b.profile.gridX) {
           return a.profile.gridY - b.profile.gridY;
         } else {
@@ -39,23 +36,18 @@ export const FieldAudioOverviewComponent = ({
     profiles ? profiles : []
   );
   const [isFlipped, setIsFlipped] = React.useState<boolean>(false);
-  const getReplacementPlayers = (list: any[]) => {
-    const filtertedList = list.filter(
-      (player) => player.gridX === -1 && player.gridY === -1
+  const getReplacementPlayers = () => {
+    const filtertedList = playersList.filter(
+      (player) => player.profile.gridX === -1 && player.profile.gridY === -1
     );
     return filtertedList;
   };
-  const [swapPlayersList, setSwapPlayersList] = React.useState<any[]>(
-    getReplacementPlayers(profiles)
-  );
-  const [fieldData, setFieldData] =
-    React.useState<IFieldOverview>(fieldOverview);
   const [currentPlayer, setCurrentPlayer] = React.useState<any>(
     playersList[0] && playersList[0]
   );
   const [formation, setFormation] = React.useState<string>(
-    fieldData.matchData.currentFormation
-      ? fieldData.matchData.currentFormation
+    fieldOverview.matchData.currentFormation
+      ? fieldOverview.matchData.currentFormation
       : ""
   );
   const [sliceFrom, setSliceFrom] = React.useState(0);
@@ -67,9 +59,6 @@ export const FieldAudioOverviewComponent = ({
     const width = newFormation.charAt(position - 1);
     return Math.round(100 / parseInt(width));
   };
-  const formations: string[] = React.useMemo(() => {
-    return fieldData.formations;
-  }, [fieldData.formations]);
 
   const updatePlayers = (playerTarget: any, isReclacement?: boolean) => {
     const players = playersList.map((player, index) => {
@@ -89,23 +78,10 @@ export const FieldAudioOverviewComponent = ({
       return { ...player, profile: playerCopy };
     });
     if (players) {
-      const swapPlayers = getReplacementPlayers(players);
-      setSwapPlayersList(swapPlayers);
       const sortedList = sortPlayers(players);
-      setIsLoading(true);
       setPlayersList(sortedList);
-      setIsLoading(false);
     }
   };
-
-  // const changeFormation = () => {
-  //   setIsLoading(true);
-  //   const sortedResult = sortPlayer(playersList);
-  //   setPlayersList(sortedResult);
-  //   if (sortedResult) {
-  //     setIsLoading(false);
-  //   }
-  // };
 
   React.useEffect(() => {
     setIsLoading(true);
@@ -117,7 +93,7 @@ export const FieldAudioOverviewComponent = ({
   }, []);
 
   return (
-    fieldData && (
+    fieldOverview && (
       <div className={style["field-audio-overview"]}>
         {!isLoading ? (
           <div
@@ -139,41 +115,44 @@ export const FieldAudioOverviewComponent = ({
               style={formation === "Training" ? { paddingTop: "44px" } : {}}
             >
               {playersList &&
-                playersList.map(
-                  (player, key) =>
-                    player.profile.gridX !== undefined && (
-                      <div
-                        key={key}
-                        style={
-                          formation !== "Training"
-                            ? {
-                                width:
-                                  key === 0
-                                    ? "100%"
-                                    : `${getPlayerPosition(
-                                        player.profile.gridX
-                                      )}% `,
-                              }
-                            : { width: "20%" }
-                        }
+                playersList
+                  .filter(
+                    (player) =>
+                      player.profile.gridX > -1 &&
+                      player.profile.gridX !== undefined
+                  )
+                  .map((player, key) => (
+                    <div
+                      key={key}
+                      style={
+                        formation !== "Training"
+                          ? {
+                              width:
+                                key === 0
+                                  ? "100%"
+                                  : `${getPlayerPosition(
+                                      player.profile.gridX
+                                    )}% `,
+                            }
+                          : { width: "20%" }
+                      }
+                    >
+                      <PlayerComponent
+                        player={player.profile}
+                        label={0}
+                        value={0}
+                        color={""}
+                        onPlayerDrag={setCurrentPlayer}
+                        onPlayerDrop={updatePlayers}
                       >
-                        <PlayerComponent
-                          player={player.profile}
-                          label={0}
-                          value={0}
-                          color={""}
-                          onPlayerDrag={setCurrentPlayer}
-                          onPlayerDrop={updatePlayers}
-                        >
-                          <AudioPlayerComponent
-                            audios={player.audioRecordingData}
-                            currentTime={currentTime}
-                            isPlaying={isPlaying}
-                          />
-                        </PlayerComponent>
-                      </div>
-                    )
-                )}
+                        <AudioPlayerComponent
+                          audios={player.audioRecordingData}
+                          currentTime={currentTime}
+                          isPlaying={isPlaying}
+                        />
+                      </PlayerComponent>
+                    </div>
+                  ))}
             </div>
           </div>
         ) : (
@@ -184,116 +163,50 @@ export const FieldAudioOverviewComponent = ({
 
         <div className={style["field-audio-overview-bottom"]}>
           <div>
-            {swapPlayersList.length > 8 && (
+            {getReplacementPlayers().length > 4 && (
               <ReactSVG
                 src="/icons/arrow-down.svg"
                 style={{
                   transform: "rotate(90deg)",
                   opacity: sliceFrom === 1 ? 0.3 : 1,
                 }}
-                onClick={() => sliceFrom > 1 && setSliceFrom(sliceFrom - 8)}
+                onClick={() => sliceFrom > 1 && setSliceFrom(sliceFrom - 4)}
               />
             )}
             <div className={style["field-audio-overview-swaps"]}>
-              {swapPlayersList
-                .slice(sliceFrom, sliceFrom + 8)
-                .map((player, key) => (
-                  <PlayerComponent
-                    player={player.profile}
-                    label={0}
-                    value={0}
-                    color={""}
-                    onPlayerDrag={(index) => setCurrentPlayer(index)}
-                    onPlayerDrop={updatePlayers}
-                    key={key}
-                  />
-                ))}
+              {playersList
+                .slice(sliceFrom, sliceFrom + 4)
+                .map(
+                  (player, key) =>
+                    player.profile.gridX < 0 && (
+                      <PlayerComponent
+                        player={player.profile}
+                        label={0}
+                        value={0}
+                        color={""}
+                        onPlayerDrag={(index) => setCurrentPlayer(index)}
+                        onPlayerDrop={(player) => updatePlayers(player, true)}
+                        key={key}
+                      />
+                    )
+                )}
             </div>
 
-            {/* {swapPlayersList.length > 8 && (
-                <ReactSVG
-                  src="/icons/arrow-down.svg"
-                  style={{
-                    transform: "rotate(270deg)",
-                    opacity: sliceFrom === swapPlayersList.length - 8 ? 0.3 : 1,
-                  }}
-                  onClick={() =>
-                    sliceFrom < swapPlayersList.length - 8 &&
-                    setSliceFrom(sliceFrom + 8)
-                  }
-                />
-              )} */}
-          </div>
-          {/* <div className={style["field-overview-gradient"]}>
-            <div>
-              <span>Few</span>
-              <span>Many</span>
-            </div>
-            <div>
-              {colors.map((color, key) => (
-                <span
-                  key={key}
-                  style={{
-                    width: `${100 / colors.length}%`,
-                    backgroundColor: color,
-                  }}
-                ></span>
-              ))}
-            </div>
-          </div> 
-            <div className={style["field-overview-buttons"]}>
-              <div>
-              <input
-                placeholder="Start"
-                type={"number"}
-                max={fieldData.matchData.endMinute - 1}
-                min={0}
-                value={range.from && range.from}
-                onChange={(e: any) =>
-                  setRange({ from: e.target.value, to: range.to })
-                }
-              />
-              <input
-                placeholder="End"
-                type={"number"}
-                min={1}
-                value={range.to && range.to}
-                max={fieldData.matchData.endMinute}
-                onChange={(e: any) =>
-                  setRange({ from: range.from, to: e.target.value })
-                }
-              />
-              <ButtonComponent
-                title="Entire session"
-                hasBorder
+            {getReplacementPlayers().length > 4 && (
+              <ReactSVG
+                src="/icons/arrow-down.svg"
+                style={{
+                  transform: "rotate(270deg)",
+                  opacity:
+                    sliceFrom === getReplacementPlayers().length - 4 ? 0.3 : 1,
+                }}
                 onClick={() =>
-                  setRange({
-                    from: fieldData.matchData.startMinute,
-                    to: fieldData.matchData.endMinute,
-                  })
+                  sliceFrom < getReplacementPlayers().length - 4 &&
+                  setSliceFrom(sliceFrom + 4)
                 }
               />
-            </div> 
-              <div>
-                {fieldData.dataSets.map((data, key) => (
-                  <ButtonComponent
-                    title={data.name}
-                    icon={`/icons/` + data.icon}
-                    key={key}
-                    variant={
-                      selectedButton === data.name ? "transparent" : "disabled"
-                    }
-                    hasBorder
-                    position="top"
-                    onClick={() =>
-                      setSelectedButton(data.name ? data.name : "")
-                    }
-                  />
-                ))}
-              </div>
-            </div>
+            )}
           </div>
-        )} */}
         </div>
       </div>
     )
